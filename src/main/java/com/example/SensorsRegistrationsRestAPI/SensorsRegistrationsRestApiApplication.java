96784d920f5d65e35580f4c06adaa5ea97175dbd
DenWorker <whiteskyD@yandex.ru>
2023-05-20 12:16:24 +0300

    Create application

---
+++ src/main/java/com/example/SensorsRegistrationsRestAPI/SensorsRegistrationsRestApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.SensorsRegistrationsRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorsRegistrationsRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorsRegistrationsRestApiApplication.class, args);
+
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ src/test/java/com/example/SensorsRegistrationsRestAPI/SensorsRegistrationsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SensorsRegistrationsRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorsRegistrationsRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
